69e84bfb86533387e672a6e0c22edd692835076d
Acinonyx <acinonyxs@yahoo.gr>
2009-07-04 16:13:19 +0300

    Major overhaul and cleanup on read and parse functions
    Conflicts:
    
    	src/olsrd_plugin.c

--- src/olsrd_plugin.c
+++ src/olsrd_plugin.c
@@ -36,9 +36,7 @@
 static void __attribute__ ((constructor)) my_init(void);
 static void __attribute__ ((destructor)) my_fini(void);
 
-#if 0
 static set_plugin_parameter set_redistribute;
-#endif
 static set_plugin_parameter set_exportroutes;
 static set_plugin_parameter set_distance;
 static set_plugin_parameter set_localpref;
@@ -49,9 +47,7 @@ int olsrd_plugin_interface_version (void) {
 }
 
 static const struct olsrd_plugin_parameters plugin_parameters[] = {
-#if 0
   { .name = "redistribute", .set_plugin_parameter = &set_redistribute, },
-#endif
   { .name = "ExportRoutes", .set_plugin_parameter = &set_exportroutes, },
   { .name = "Distance",     .set_plugin_parameter = &set_distance,     },
   { .name = "LocalPref",    .set_plugin_parameter = &set_localpref,    },
@@ -63,7 +59,6 @@ void olsrd_get_plugin_parameters (const struct olsrd_plugin_parameters **params,
   *size = ARRAYSIZE(plugin_parameters);
 }
 
-#if 0
 static int set_redistribute (const char *value,
 			     void *data __attribute__((unused)),
 			     set_plugin_parameter_addon addon __attribute__((unused))) {
@@ -73,14 +68,12 @@ static int set_redistribute (const char *value,
   unsigned int i;
 
   for (i = 0; i < ARRAYSIZE(zebra_route_types); i++) {
-    if (!strcmp(value, zebra_route_types[i])) {
-      zebra_redistribute(i);
-      return 0;
-    }
+    if (!strcmp(value, zebra_route_types[i]))
+      if (zebra_redistribute (i)) return 1;
   }
-  return 1;
+
+  return 0;
 }
-#endif
 
 static int set_exportroutes (const char *value,
 			     void *data __attribute__((unused)),
@@ -125,10 +118,8 @@ int olsrd_plugin_init(void) {
     return 1;
   }
 
-#if 0
   olsr_start_timer(1 * MSEC_PER_SEC, 0, OLSR_TIMER_PERIODIC,
-                   &zebra_check, NULL, 0);
-#endif
+                   &zebra_parse, NULL, 0);
 
   return 0;
 }
